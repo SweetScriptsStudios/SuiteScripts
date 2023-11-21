abefea21488e4a6c2c5cb025ee1b7ec074799755
SweetScriptsStudios <151624548+SweetScriptsStudios@users.noreply.github.com>
2023-11-21 18:15:03 +0530

    Update MassDelete_using_BrowserConsole.js

--- MassDelete_using_BrowserConsole.js
+++ MassDelete_using_BrowserConsole.js
@@ -1,20 +1,21 @@
 require(["N/record"], function(record) {
 
     var arrayOfIds = [1, 2, 3]
+    var recordType = 'salesorder';
 
     // Using forEach to loop through the array
     arrayOfIds.forEach((id) => {
         try {
             // using record.delete API to DELETE
             var objRecordId = record.delete({
-                type: 'salesorder',
+                type: recordType,
                 id: id
             });
             // Log the ID of the deleted record
             console.log(`Deleted Record ID: ${objRecordId}`);
         } catch (error) {
             // Log any errors that occur during the deletion process
-            console.error(`Error in Deleting the record with Type : ${params.type} and Id : ${params.id}`, error.message);
+            console.error(`Error in Deleting the record with Type : ${recordType} and Id : ${id}`, error.message);
         }
 
     });
