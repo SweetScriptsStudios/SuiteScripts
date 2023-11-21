0a6da56fc0eb2b82c082aeaca7277f646e088859
SweetScriptsStudios <151624548+SweetScriptsStudios@users.noreply.github.com>
2023-11-21 15:59:19 +0530

    Update MassDelete_using_BrowserConsole.js

--- MassDelete_using_BrowserConsole.js
+++ MassDelete_using_BrowserConsole.js
@@ -1,6 +1,20 @@
 require(["N/record"], function(record) {
 	
-    var s = search.load({id:"572", type:"transaction"});
-    // code goes here
+  	 try {
+                // Log the parameters for debugging purposes
+                log.debug("params ", JSON.stringify(params));
+
+                // Load the record to be deleted
+                var objRecordId = record.delete({
+                    type: params.type,
+                    id: params.id
+                });
+
+                // Log the ID of the deleted record
+                log.debug("Deleted Record with Id ", objRecordId);
+            } catch (error) {
+                // Log any errors that occur during the deletion process
+                log.debug(`Error in Deleting the record with Type : ${params.type} and Id : ${params.id}`, error.message);
+            }
 	
 })
